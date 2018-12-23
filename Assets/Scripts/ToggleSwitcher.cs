231e127131c637d6a0de061d0bbcd05087ef1b91
pgruszczynski93 <pgruszczynski93@gmail.com>
2018-12-23 18:40:24 +0100

    Toggle switch added

---
+++ Assets/Scripts/ToggleSwitcher.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace InstaGen
+{
+	public class ToggleSwitcher : MonoBehaviour
+	{
+		[SerializeField] private int _toggleIndex;
+		[SerializeField] private int _toggleCount;
+		[SerializeField] private Toggle[] _sideToggles;
+
+		private void SetReferences()
+		{
+			_toggleCount = _sideToggles.Length;
+		}
+
+		private void Start()
+		{
+			SetReferences();
+		}
+
+		public void SetNextToggle()
+		{
+			if (_toggleIndex < _toggleCount)
+			{
+				_sideToggles[_toggleIndex].isOn = false;
+				_sideToggles[++_toggleIndex].isOn = true;
+			}
+		}
+
+		public void SetPreviousToggle()
+		{
+			if (_toggleIndex > 0)
+			{
+				_sideToggles[_toggleIndex].isOn = false;
+				_sideToggles[--_toggleIndex].isOn = true;
+			}
+		}
+	}
+}
